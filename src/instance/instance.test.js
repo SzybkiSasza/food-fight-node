349a11e9d9bc38a11e331b305f8494fee2a179fb
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2018-03-26 01:28:51 +0300

    WIP on re-enabling tests for instance.

--- src/instance/instance.test.js
+++ src/instance/instance.test.js
@@ -3,7 +3,7 @@ jest.mock('./schemas/config');
 
 // import * as transports from '../transports';
 
-// import configSchema from './schemas/config';
+import configSchema from './schemas/config';
 import Instance from './instance';
 
 describe('Instance class tests', () => {
@@ -11,41 +11,41 @@ describe('Instance class tests', () => {
     expect(Instance).toBeInstanceOf(Function);
   });
 
-  // describe('Constructor', () => {
-  //   it('Throw an error if config is not valid', () => {
-  //     configSchema.validate.mockImplementationOnce(() => ({
-  //       error: new Error('Some Validation Error!'),
-  //     }));
-  //
-  //     try {
-  //       new Instance({
-  //         entityName: 'EN',
-  //       });
-  //
-  //       throw new Error('This should not be thrown');
-  //     } catch (err) {
-  //       expect(err).toBeInstanceOf(Error);
-  //       expect(err.message).toEqual('Some Validation Error!');
-  //     }
-  //   });
-  //
-  //   it('Sets the config and initialization flag on the constructions', () => {
-  //     const config = {
-  //       entityName: 'EN',
-  //     };
-  //
-  //     configSchema.validate.mockImplementationOnce(() => ({
-  //       value: config,
-  //     }));
-  //
-  //     const instance = new Instance(config);
-  //
-  //     expect(instance.isInitialized).toEqual(false);
-  //     expect(instance.config).toEqual(config);
-  //   });
-  // });
-  //
-  // describe('Init', () => {
+  describe('Constructor', () => {
+    it('Throw an error if config is not valid', () => {
+      configSchema.validate.mockImplementationOnce(() => ({
+        error: new Error('Some Validation Error!'),
+      }));
+
+      try {
+        new Instance({
+          entityName: 'EN',
+        });
+
+        throw new Error('This should not be thrown');
+      } catch (err) {
+        expect(err).toBeInstanceOf(Error);
+        expect(err.message).toEqual('Some Validation Error!');
+      }
+    });
+
+    it('Sets the config and initialization flag on the constructions', () => {
+      const config = {
+        entityName: 'EN',
+      };
+
+      configSchema.validate.mockImplementationOnce(() => ({
+        value: config,
+      }));
+
+      const instance = new Instance(config);
+
+      expect(instance.isInitialized).toEqual(false);
+      expect(instance.config).toEqual(config);
+    });
+  });
+
+  describe('Init', () => {
   //   it('Throws if transport for the config does not exist', async () => {
   //     const config = {
   //       value: {
@@ -108,7 +108,7 @@ describe('Instance class tests', () => {
   //     expect(instance.transports.direct).toBeInstanceOf(Object);
   //     expect(instance.isInitialized).toEqual(true);
   //   });
-  // });
+  });
   //
   // describe('Methods', () => {
   //   const oldConsoleWarn = console.warn;
